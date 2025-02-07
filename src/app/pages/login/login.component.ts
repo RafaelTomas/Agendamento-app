9d20c30246051a832a1d93734f2d2920b000ae98
Rafael Tomas <rafael.tomas@mv.com.br>
2025-02-07 11:51:03 -0300

    feature(login): create login styles

---
+++ src/app/pages/login/login.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms';
+
+
+@Component({
+  selector: 'app-login',
+  imports: [],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.css'
+})
+export class LoginComponent implements OnInit {
+
+  loginForm!: FormGroup;
+
+  constructor(private formBuilder: FormBuilder) { }
+
+  ngOnInit(): void {
+      this.createForm();
+  }
+
+  createForm(){
+    this.loginForm = this.formBuilder.group({
+      email: new FormControl('', [Validators.required, Validators.email]),
+      password: new FormControl('', [Validators.required, Validators.minLength(6)])
+    });
+  }
+  login() { console.info(this.loginForm.value)}
+}
