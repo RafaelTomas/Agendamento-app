fe05c110b5e0632f89638e850271e36ecc64d170
Rafael Tomas <rafael.tomas@mv.com.br>
2025-02-09 14:41:37 -0300

    feat(core): add guard router

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,10 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
 import { ContactsComponent } from './pages/contacts/contacts.component';
+import { AuthGuard } from './core/guards/auth.guard';
 
 export const routes: Routes = [
-    { path: 'contatos', component: ContactsComponent },
+    { path: 'contatos', component: ContactsComponent, canActivate: [AuthGuard] },
     { path: 'login', component: LoginComponent },
     { path: '', redirectTo: '/login', pathMatch: 'full' },
     { path: '**', redirectTo: '/login' }
